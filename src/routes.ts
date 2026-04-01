77a552c7fef8050c97d1752fe1947493a2fb8b5c
Sri Krishna Sai Kota <srikrishnasaikota@gmail.com>
2026-03-31 22:49:04 -0400

    Fix GitHub Pages routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router';
+/*import { createBrowserRouter } from 'react-router';
 import { Home } from './pages/Home';
 import { LotDetail } from './pages/LotDetail';
 import { NotFound } from './pages/NotFound';
@@ -7,4 +7,15 @@ export const router = createBrowserRouter([
   { path: '/',          Component: Home },
   { path: '/lot/:lotId', Component: LotDetail },
   { path: '*',          Component: NotFound },
+]);*/
+
+import { createHashRouter } from 'react-router';
+import { Home } from './pages/Home';
+import { LotDetail } from './pages/LotDetail';
+import { NotFound } from './pages/NotFound';
+
+export const router = createHashRouter([
+  { path: '/', Component: Home },
+  { path: '/lot/:lotId', Component: LotDetail },
+  { path: '*', Component: NotFound },
 ]);
